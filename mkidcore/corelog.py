1923889d0a8b32757c62aadfbc731343755ff882
Jeb Bailey <baileyji@umich.edu>
2018-12-04 16:06:34 -0800

    tweak console type handler checking

--- mkidcore/corelog.py
+++ mkidcore/corelog.py
@@ -91,7 +91,8 @@ def create_log(name, logfile='', console=True, mpsafe=True, propagate=False,
         handler.setFormatter(logging.Formatter(fmt))
         handler.setLevel(level)
         log.addHandler(handler)
-    if console and not len(filter(lambda h: isinstance(h, logging.StreamHandler), log.handlers)):
+
+    if console and not len(filter(lambda h: type(h) == logging.StreamHandler, log.handlers)):
         handler = logging.StreamHandler(sys.stdout if stdout else sys.stderr)
         handler.setFormatter(logging.Formatter(fmt))
         handler.setLevel(level)
