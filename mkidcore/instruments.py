9b6df37f6b3cd52cb379c9466f785d41bc776c0e
Jeb Bailey <baileyji@umich.edu>
2019-05-29 17:09:55 -0700

    Disable MEC roach with PPS issue

--- mkidcore/instruments.py
+++ mkidcore/instruments.py
@@ -9,8 +9,8 @@ DARKNESS_FEEDLINE_INFO = dict(num=5, width=25, length=80)
 DEFAULT_ARRAY_SIZES = {'mec': (140, 146), 'darkness': (80, 125)}
 
 
-MEC_NUM_FL_MAP = {236: '1a', 237: '1b', #238: '5a', 239: '5b',
-                  220: '6a', 221: '6b', 222: '7a', 223: '7b', 232: '8a',
+MEC_NUM_FL_MAP = {236: '1a', 237: '1b', #238: '5a', 239: '5b',220: '6a',
+                  221: '6b', 222: '7a', 223: '7b', 232: '8a',
                   233: '8b', 228: '9a', 229: '9b', 224: '10a', 225: '10b'}
 
 #NB FLs are arbitrary as instrument isn't installed
